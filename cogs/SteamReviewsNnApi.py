7f4d4a98c6cec34f967f586f164bbf8789fa7ef6
Данил Долгов <spectrefromthefuture@gmail.com>
2024-02-02 01:00:28 +0400

    sdf

--- cogs/SteamReviewsNnApi.py
+++ cogs/SteamReviewsNnApi.py
@@ -1,22 +1,42 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from keras.preprocessing.text import Tokenizer
+import pickle
+import json
+import requests
+
 
 class SteamReviewsNnApi(commands.Cog):
 
+    def get_tokenizer(self, pickle_file_path: str) -> Tokenizer:
+        with open(pickle_file_path, 'rb') as handle:
+            tokenizer = pickle.load(handle)
+        return tokenizer
+
+
     def __init__(self, client: commands.Bot):
         self.client = client
-    
+        self.tokenizer = self.get_tokenizer("Tokenizer/tokenizer.pickle")
+
+
+    def get_predictions(self, prompt: str):
+        
+        sequences = self.tokenizer.texts_to_sequences([prompt])
+        data_to_send = json.dumps({'instances': sequences})
+        model_url = f"http://localhost:8605/v1/models/AAAAAAAAAAAAAAAAAAAAAA:predict"
+        predictions = requests.post(model_url, data=data_to_send)
+        return predictions
+
+
 
     @app_commands.command(name='predict', description="Predicts how you would rank some steam game based on your text.")
-    async def preidct(self, interaction: discord.Interaction, text: str):
-        await interaction.response.send_message(f"If you're seeing this then the predict command works! Also, here is the text you've just entered: {text}")
+    async def preidct(self, interaction: discord.Interaction, prompt: str):
+        predictions = self.get_predictions(prompt)
+
+        await interaction.response.send_message(f"Predictions: {predictions}")
 
 
 async def setup(client: commands.Bot) -> None:  
-        await client.add_cog(SteamReviewsNnApi(client))
+    await client.add_cog(SteamReviewsNnApi(client))
     
-    
-        
-    
-
